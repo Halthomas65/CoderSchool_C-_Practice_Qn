6c8e0a49a716d442288b7c124e768ee18d6a89ed
Halthomas65 <thaihao.dp@gmail.com>
2024-06-22 09:21:19 +0700

    L1_Intro
    done all 3 parts

--- L1_Introduction/Programming_C#_101/Programming_C#_101/Program.cs
+++ L1_Introduction/Programming_C#_101/Programming_C#_101/Program.cs
@@ -1,11 +1,18 @@
 ﻿//Part 1 Print a string to the console
-//hint: Console.WriteLine("Hello World!");
+Console.WriteLine("Part 1\nHello World!");
 
 //Part 2 Get user input and print it to the console
-//hint: Console.ReadLine();
+Console.WriteLine("\nPart 2\nEnter something: ");
+var thing = Console.ReadLine();
+Console.WriteLine($"You typed: '{thing}'");
+
 
 //Part 3 Get two inputs from the user and print them on separate lines
 //hint: use \n to create a new line
+Console.WriteLine("\nPart 3\nEnter 2 line of something: ");
+var line1 = Console.ReadLine();
+var line2 = Console.ReadLine();
+Console.WriteLine($"You typed:\n{line1}\n{line2}");
 
 
 
